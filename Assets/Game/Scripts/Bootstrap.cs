2f108dcc49f23cea343e27c51dcfbb3b9bc55713
Valentyn Romanyshyn <valentyn001@gmail.com>
2025-08-23 12:29:23 +0200

    imported packages
    - upd gitignore
    - added loading screen asset

---
+++ Assets/Game/Scripts/Bootstrap.cs
@@ -0,0 +1,20 @@
+﻿using Michsky.LSS;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Game.Scripts
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        [SerializeField] private LSS_LoadingScreen _loadingScreen;
+        
+        private async void Awake()
+        {
+            _loadingScreen.enableVirtualLoading = true;
+            _loadingScreen.onLoadingStart.Invoke();
+
+            var handle = Addressables.InitializeAsync();
+            await handle.Task;
+        }
+    }
+}
