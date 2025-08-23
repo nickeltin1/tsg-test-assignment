302be3995cddd4eb9cada0100cfa813dfdc50f86
Valentyn Romanyshyn <valentyn001@gmail.com>
2025-08-23 12:39:45 +0200

    generated addresables data

--- Assets/Game/Scripts/Bootstrap.cs
+++ Assets/Game/Scripts/Bootstrap.cs
@@ -10,11 +10,16 @@ namespace Game.Scripts
         
         private async void Awake()
         {
-            _loadingScreen.enableVirtualLoading = true;
-            _loadingScreen.onLoadingStart.Invoke();
+            // _loadingScreen.enableVirtualLoading = true;
+            // _loadingScreen.onLoadingStart.Invoke();
 
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            Debug.Log("Addressables init started");
             var handle = Addressables.InitializeAsync();
             await handle.Task;
+            stopwatch.Stop();
+            Debug.Log($"Addressables init ended, time took {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
